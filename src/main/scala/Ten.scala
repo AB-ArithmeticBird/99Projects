cf160908a3bba59b6cae328cffc33eb4239c74bb
arithmeticbird <arithmeticbird@gmail.com>
2016-07-24 01:27:25 +0530

    Third Problem

--- src/main/scala/Ten.scala
+++ src/main/scala/Ten.scala
@@ -1,28 +1,36 @@
 import scala.annotation.tailrec
 
-object Main extends App{
+object Main extends App {
   println("hello world")
-  val list = Range(0,10).toList
+  val list = Range(0, 10).toList
   println("last item is:%s".format(last(list)))
   println("Last two element is: %s".format(last_two(list)))
+  println("kth element is:" + at(list, 4))
 
   //Write a function last : 'a list -> 'a option that returns the last element of a list. (easy)
   @tailrec
-  def last[A](list:List[A]): Option[A] =  list match{
+  def last[A](list: List[A]): Option[A] = list match {
     case Nil => None
-    case h::Nil => Some(h)
-    case h::t => last (t)
+    case h :: Nil => Some(h)
+    case h :: t => last(t)
   }
 
   //Find the last but one (last and penultimate) elements of a list. (easy)
   @tailrec
-  def last_two[A] (list: List[A]): (Option[A], Option[A]) = list match {
+  def last_two[A](list: List[A]): (Option[A], Option[A]) = list match {
     case Nil => (None, None)
     case h :: t => t match {
       case Nil => (Some(h), None)
       case th :: Nil => (Some(h), Some(th))
       case th :: tt => last_two(t)
     }
+  }
+
+  //Find the k'th element of a list. (easy)
+  @tailrec
+  def at[A](list: List[A], n: Int): Option[A] = list match {
+    case Nil => None
+    case h :: t => if (n == 0) Some(h) else at(t, n - 1)
   }
 
 }
